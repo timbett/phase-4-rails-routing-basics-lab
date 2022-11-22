0cfbba5b17f94fe71824914de4c913e9f292440a
Timothy Kibet <tkkibet13@gmail.com>
2022-11-22 13:00:47 +0300

    Assignment Complete

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+    def index
+        students=Student.all
+        render json:students
+    end
+    def grades
+        students=Student.all.order(grade: :DESC)
+        render json: students
+    end
+end
